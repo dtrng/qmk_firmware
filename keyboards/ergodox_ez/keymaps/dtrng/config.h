8c06e7e94d7abdbdff5629752e1013cca5f42695
David Trang <david.a.trang@gmail.com>
2018-12-01 15:41:42 +0100

    added first trial

---
+++ keyboards/ergodox_ez/keymaps/dtrng/config.h
@@ -0,0 +1,7 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define AUTO_SHIFT_TIMEOUT 150
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_ALPHA
