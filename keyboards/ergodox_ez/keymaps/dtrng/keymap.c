c27d513da8468932c03df3115a996002863551e7
David Trang <david.a.trang@gmail.com>
2019-10-12 21:14:27 +0200

    wip

--- keyboards/ergodox_ez/keymaps/dtrng/keymap.c
+++ keyboards/ergodox_ez/keymaps/dtrng/keymap.c
@@ -30,25 +30,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox(
       // left hand
       KC_EQUAL,              KC_1,            KC_2,     KC_3,           KC_4,           KC_5,   KC_LEFT,
-      KC_ESCAPE,             KC_Q,            KC_W,     KC_E,           KC_R,           KC_T,   TG(1),
+      KC_TAB,                KC_Q,            KC_W,     KC_E,           KC_R,           KC_T,   TG(1),
       CTL_T(KC_ESCAPE),      KC_A,            KC_S,     KC_D,           KC_F,           KC_G,
       SFT_T(KC_TILD),        CTL_T(KC_Z),     KC_X,     KC_C,           KC_V,           KC_B,   MT(MOD_HYPR, KC_NO), 
-      LT(1,KC_GRAVE),        ALT_T(KC_QUOTE), KC_LGUI,  MO(2),          MO(1), 
+      LT(1,KC_GRAVE),        ALT_T(KC_QUOTE), MO(2),    MO(1),          KC_LGUI,   
 
                                                                  ALT_T(KC_APPLICATION), KC_LGUI,
                                                                                         KC_HOME,
-                                                 KC_SPACE,       KC_BSPACE,             KC_END,    
+                                                 KC_SPACE,       KC_BSPACE,             MO(1),    
 
       // right hand
       KC_RIGHT,           KC_6,            KC_7,          KC_8,          KC_9,     KC_0,             KC_MINUS,
       TG(1),              KC_Y,            KC_U,          KC_I,          KC_O,     KC_P,             KC_BSLASH,
                           KC_H,            KC_J,          KC_K,          KC_L,     LT(2,KC_SCOLON),  GUI_T(KC_QUOTE),
       MT(MOD_MEH, KC_NO), KC_N,            KC_M,          KC_COMMA,      KC_DOT,   RCTL_T(KC_SLASH), KC_RSHIFT,
-                                           MO(1),         MO(2),         KC_RGUI,  KC_LALT,          MO(1),
+                                           KC_RGUI,       MO(1),         MO(2),    KC_LALT,          MO(1),
 
       KC_LALT,            CTL_T(KC_ESCAPE),
       KC_PGUP,
-      KC_PGDOWN,          KC_TAB,             KC_ENTER
+      MO(1),              KC_TAB,             KC_ENTER
   ),
 
   [1] = LAYOUT_ergodox(
